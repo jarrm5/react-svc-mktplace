380047724d58f878aafb94fe54c9638db0b0ee7f
James Rinella <rinellaj2448@gmail.com>
2024-06-08 06:45:12 -0500

    add form and services array

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+const services = [
+  {
+    company: "Bob Hamilton Heating and Cooling",
+    tags: ["heating","cooling","hvac"]
+  },
+  {
+    company: "Eden Tree Service",
+    tags: ["lawncare","tree pruning"]
+  }
+]
 
 
 export default function App() {
-  return "App component";
+  return (
+    <>
+    <form method="GET">
+<div><label htmlFor="">What do you need done?</label><input placeholder="I want to.." type="text" /></div>
+<button type="submit">Search</button>
+    </form>
+    <div className="results">
+      
+    </div>
+    </>
+  )
 }
 
