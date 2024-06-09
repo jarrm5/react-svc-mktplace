aa708f4111a706107aae8525764006e3d2d8755b
James Rinella <rinellaj2448@gmail.com>
2024-06-09 07:26:43 -0500

    basic form styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
 import { useState } from "react";
 import SearchResultList from "./SearchResultList";
 
-const services = [
-  {
-    company: "Bob Hamilton Heating and Cooling",
-    tags: ["heating","cooling","hvac"]
-  },
-  {
-    company: "Eden Tree Service",
-    tags: ["lawncare","tree pruning"]
-  }
-];
-
-
 export default function App() {
-
-  const[search,setSearch] = useState("");
+  const [search, setSearch] = useState("");
 
   return (
     <>
-    <form method="GET">
-      <div>
-        <label htmlFor="">What do you need done?</label><input placeholder="I want to.." type="text" />
+      <form className="flex-column-center-gap" method="GET">
+        <label htmlFor="">What do you need done?</label>
+        <div>
+          <input placeholder="I want to.." type="text" />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+      <div className="results flex-column-center-gap">
+        <SearchResultList></SearchResultList>
       </div>
-      <button type="submit">Search</button>
-    </form>
-    <div className="results">
-      <SearchResultList></SearchResultList>
-    </div>
     </>
-  )
+  );
 }
-
--- src/SearchResultList.jsx
+++ src/SearchResultList.jsx
@@ -1,7 +1,19 @@
+const services = [
+  {
+    company: "Bob Hamilton Heating and Cooling",
+    tags: ["heating", "cooling", "hvac"],
+  },
+  {
+    company: "Eden Tree Service",
+    tags: ["lawncare", "tree pruning"],
+  },
+];
+
 export default function SearchResult() {
-    return (
-        <ul>
-            <li>foo</li>
-        </ul>
-    )
+  return (
+    <ul>
+      <li>foo</li>
+      <li>bar</li>
+    </ul>
+  );
 }
