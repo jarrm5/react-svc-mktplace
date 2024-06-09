d1f0a04f67910c6d3363978bf2734a3a3ecf2f6f
James Rinella <rinellaj2448@gmail.com>
2024-06-09 07:09:11 -0500

    add search result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import SearchResultList from "./SearchResultList";
+
 const services = [
   {
     company: "Bob Hamilton Heating and Cooling",
@@ -7,18 +10,23 @@ const services = [
     company: "Eden Tree Service",
     tags: ["lawncare","tree pruning"]
   }
-]
+];
 
 
 export default function App() {
+
+  const[search,setSearch] = useState("");
+
   return (
     <>
     <form method="GET">
-<div><label htmlFor="">What do you need done?</label><input placeholder="I want to.." type="text" /></div>
-<button type="submit">Search</button>
+      <div>
+        <label htmlFor="">What do you need done?</label><input placeholder="I want to.." type="text" />
+      </div>
+      <button type="submit">Search</button>
     </form>
     <div className="results">
-      
+      <SearchResultList></SearchResultList>
     </div>
     </>
   )
